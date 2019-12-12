6e9d4e01f3f4b04c4f4ffb2c374613216cef7225
Gilberto Acosta <acostagilberto95@gmail.com>
2019-12-12 16:19:31 -0400

    Tipos de datos

--- seccion-3/app.js
+++ seccion-3/app.js
@@ -1,2 +1,14 @@
 
-console.log("Hola Mundo!");
+// console.log("Hola Gilbert");
+
+let c;
+let f = 90;
+
+// c = (f - 32) * (5/9); // primara solucion
+
+// segunda solucion
+c = f - 32;
+
+c = c * (5/9);
+
+console.log(c);
---
+++ seccion-4/app.js
@@ -0,0 +1,20 @@
+let nombre = "Gilbert";
+let apellido = "Acosta";
+
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto);
+
+console.log("-----------------");
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
+
+console.log("-----------------");
+
+let activo = true;
+let despedido = false;
+
+console.log(activo + despedido);
