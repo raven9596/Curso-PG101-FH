68d4a81f057e72e85e6b3a3976f146d5206991ea
Gilberto Acosta <acostagilberto95@gmail.com>
2019-12-13 14:55:04 -0400

    Codiciones y ciclos
    if, else, else if, switch case, while

---
+++ seccion-4.1/app.js
@@ -0,0 +1,19 @@
+// Crear un algoritmo, que basado en la nota
+// imprima si el alumno aprueba la materia
+// aprueba con 60
+
+let nota = 55;
+
+if(nota >= 60){
+    
+    console.log("El alumno aprueba la clase");
+}
+else if (nota < 50){
+    console.log("Por favor estudie mas");
+
+}
+
+else{
+    console.log("El alumno reprobo la clase");
+
+}
---
+++ seccion-4.4/app.js
@@ -0,0 +1,14 @@
+
+
+let gasolina = 3;
+
+while(gasolina > 0){
+
+    
+    console.log("Gasolina restante " + gasolina);
+    // gasolina = gasolina - 1;
+    gasolina--;
+
+}
+
+console.log("Ya no tiene gasolina");
