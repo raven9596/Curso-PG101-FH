3f6441124b05bc3c2cbaaa66ca76586c9500c9ee
Gilbert Acosta <acostagilberto95@gmail.com>
2019-12-15 01:21:39 -0400

    Seguir en arreglos

---
+++ seccion-5.1/app.js
@@ -0,0 +1,36 @@
+function imprimirTabla(base, limite){
+    
+    for(let i = 1; i  <= limite; i++){
+    
+        let resultado = base * i;
+        console.log(base + " x "+ i + " = " + resultado);
+        
+    }
+
+}
+
+console.log("-----------------------------");
+imprimirTabla(1,12);
+console.log("-----------------------------");
+imprimirTabla(2,12);
+console.log("-----------------------------");
+imprimirTabla(3,12);
+console.log("-----------------------------");
+imprimirTabla(4,12);
+console.log("-----------------------------");
+imprimirTabla(5,12);
+console.log("-----------------------------");
+imprimirTabla(6,12);
+console.log("-----------------------------");
+imprimirTabla(7,12);
+console.log("-----------------------------");
+imprimirTabla(8,12);
+console.log("-----------------------------");
+imprimirTabla(9,12);
+console.log("-----------------------------");
+imprimirTabla(10,12);
+console.log("-----------------------------");
+imprimirTabla(11,12);
+console.log("-----------------------------");
+imprimirTabla(12,12);
+console.log("-----------------------------");
