60a8c9111f1853659ba9daef09df2c8a7fb2f047
Gilbert Acosta <acostagilberto95@gmail.com>
2019-12-18 01:41:18 -0400

    Fin del curso

--- angularapp/ahorcado/src/app/app.component.ts
+++ angularapp/ahorcado/src/app/app.component.ts
@@ -9,6 +9,10 @@ export class AppComponent {
 
   palabra = 'AGUACATE';
   palabraOculta = '';
+  intentos = 0;
+
+  gano = false;
+  perdio = false;
 
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
             'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
@@ -22,17 +26,51 @@ export class AppComponent {
 
   comprobar(letra) {
 
+    this.existeLetra(letra);
+
     const palabraOcultaArr = this.palabraOculta.split(' ');
 
-    for ( let i = 0; i < this.palabra.length; i++ ){
+    for ( let i = 0; i < this.palabra.length; i++ ) {
 
-      if(this.palabra[i] === letra){
+      if (this.palabra[i] === letra) {
         palabraOcultaArr[i] = letra;
       }
 
     }
 
     this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
   }
 
-}
+  verificaGane() {
+
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    if ( palabraEvaluar === this.palabra ) {
+      this.gano = true;
+      console.log('Usuario gano');
+    }
+
+    if ( this.intentos >= 9 ) {
+      this.perdio = true;
+      console.log('Usuario perdio');
+    }
+
+    }
+
+    existeLetra( letra ) {
+
+      if (this.palabra.indexOf(letra) >= 0 ) {
+        // console.log('letra existe ' + letra);
+      } else {
+        // console.log('Letra no existe');
+        this.intentos ++;
+      }
+    }
+
+  } // Fin de la clase
+
+
+
+
